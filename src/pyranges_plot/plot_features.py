cd7c652b6e723cf63df2067209a7c56a643e276f
endre bakken stovner <endrebakkenstovner@endres-MacBook-Air.local>
2023-12-13 12:51:20 +0100

    Ruff-formatted code

--- src/pyranges_plot/plot_features.py
+++ src/pyranges_plot/plot_features.py
@@ -1,14 +1,16 @@
 import copy
 
 plot_features_dict = {
-                      "tag_background": ("grey", "Background color of the tooltip annotation for the gene in Matplotlib.", " "),
-                      "plot_background": ("white", "Background color for the chromosomes plots.", " "),
-                      "plot_border": ("black", "Color of the line defining the chromosome plots.", " "),
-                      "title_size": (18, "Size of the plots' titles.", " "),
-                      "title_color": ("goldenrod", "Color of the plots' titles.", " "),
+    "tag_background": (
+        "grey",
+        "Background color of the tooltip annotation for the gene in Matplotlib.",
+        " ",
+    ),
+    "plot_background": ("white", "Background color for the chromosomes plots.", " "),
+    "plot_border": ("black", "Color of the line defining the chromosome plots.", " "),
+    "title_size": (18, "Size of the plots' titles.", " "),
+    "title_color": ("goldenrod", "Color of the plots' titles.", " "),
 }
 
 
 plot_features_dict_in_use = copy.deepcopy(plot_features_dict)
-
-
