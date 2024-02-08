616184aeabf0bb54158946991974dc98c18dc365
Ester Muñoz del Campo <emunozdc99@gmail.com>
2024-02-08 12:26:08 +0100

    Testing tests.

---
+++ tests/test01.py
@@ -0,0 +1,26 @@
+import pyranges as pr
+
+from pyranges_plot.data_preparation import make_subset
+
+
+def subset_test():
+    df = pr.from_dict(
+        {
+            "Chromosme": [1, 1, 2, 2, 2],
+            "Start": [10, 20, 10, 20, 30],
+            "End": [11, 21, 11, 21, 31],
+            "transcript_id": ["T1", "T2", "T3", "T4", "T5"],
+        }
+    ).df
+
+    result_subset = make_subset(df, "transcript_id", 3)
+    expected_subset = pr.from_dict(
+        {
+            "Chromosme": [1, 1, 2],
+            "Start": [10, 20, 10],
+            "End": [11, 21, 11],
+            "transcript_id": ["T1", "T2", "T3"],
+        }
+    ).df
+
+    assert len(result_subset) == len(expected_subset)
