258165bbf105d53bf5cd76b0eb6445e876824d23
Ester Muñoz del Campo <estermunozdelcampo@MBP-de-Ester.home>
2024-04-23 11:27:20 +0200

    Add figure files.

--- pr_fig_a.py
+++ pr_fig_a.py
@@ -1,16 +1,48 @@
 import pyranges as pr, pyranges_plot as prp
 
-p = pr.PyRanges(
+g = pr.PyRanges(
     {
         "Chromosome": [1, 1, 1],
         "Start": [4, 13, 19],
         "End": [10, 15, 23],
-        "transcript_id": ["a", "a", "a"],
+        "transcript_id": ["g", "g", "g"],
     }
 )
 
+g_ext = g.extend(1)
+g_ext["transcript_id"] = ["g.extend(1)"] * len(g_ext)
+g_ext_id = g.extend(1, transcript_id="transcript_id")
+g_ext_id["transcript_id"] = ["g.extend(1, id)"] * len(g_ext_id)
+
+b = g.boundaries("transcript_id")
+b["transcript_id"] = ["g.boundries"] * len(b)
+b_subseq = b.subsequence(0, 10)
+b_subseq["transcript_id"] = ["g.boundries.subseq"] * len(b_subseq)
+
+g_subseq_id = g.subsequence(0, 10, "transcript_id")
+g_subseq_id["transcript_id"] = ["g.subsequence(0,10,id)"] * len(g_subseq_id)
+g_subseq = g.subsequence(0, 3)
+g_subseq["transcript_id"] = ["g.subsequence(0,3)"] * len(g_subseq)
+
+g_spl_subseq_for = g.spliced_subsequence(1, 8, "transcript_id")
+g_spl_subseq_for["transcript_id"] = ["g.spliced_subsequence(1,8,id)"] * len(
+    g_spl_subseq_for
+)
+g_spl_subseq_rev = g.spliced_subsequence(-5, -1, "transcript_id")
+g_spl_subseq_rev["transcript_id"] = ["g.spliced_subsequence(-5,-1,id)"] * len(
+    g_spl_subseq_rev
+)
+
+
 prp.plot(
-    [p, p.extend(1), p.extend(1, transcript_id="transcript_id")],
-    engine="plt",
+    [
+        g,
+        pr.concat([g_ext, g_ext_id]),
+        pr.concat([b, b_subseq]),
+        pr.concat([g_subseq, g_subseq_id]),
+        pr.concat([g_spl_subseq_for, g_spl_subseq_rev]),
+    ],
+    engine="ply",
     id_col="transcript_id",
+    # packed=False,
 )
---
+++ pr_fig_b.py
@@ -0,0 +1,34 @@
+import pyranges as pr, pyranges_plot as prp
+
+# Start r object
+r = pr.PyRanges(
+    {
+        "Chromosome": [1, 1, 1],
+        "Start": [3, 11, 16],
+        "End": [8, 15, 19],
+        "transcript_id": ["r", "r", "r"],
+    }
+)
+
+# r.window object
+r_window = r.window(2)
+r_window["transcript_id"] = ["r.window(2)"] * len(r_window)  # rename for plot
+
+# tile_genome object
+tile_g = pr.genomicfeatures.tile_genome(
+    pr.PyRanges({"Chromosome": [1], "Start": [0], "End": [max(r["End"])]}), 2
+)
+tile_g["transcript_id"] = ["pr.tile_genome(2)"] * len(tile_g)  # rename for plot
+
+# r.tile object
+r_tile = r.tile(2)
+r_tile["transcript_id"] = ["r.tile(2)"] * len(r_tile)  # rename for plot
+
+# plot
+prp.plot(
+    [r, r_window, tile_g, r_tile],
+    engine="ply",
+    id_col="transcript_id",
+    exon_border="black",
+    # packed=False,
+)
