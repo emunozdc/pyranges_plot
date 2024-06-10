eedf342d9291fc7a168e1b6e9a9bec2940413be8
Ester Muñoz del Campo <emunozdc99@gmail.com>
2024-06-10 13:03:43 +0200

    Fix packed for 1 chr +1 pr.

--- src/pyranges_plot/data_preparation.py
+++ src/pyranges_plot/data_preparation.py
@@ -268,14 +268,25 @@ def get_genes_metadata(df, id_col, color_col, packed, colormap, v_space):
             .cumcount()
         )
 
-        if len(genesmd_df.index.get_level_values(PR_INDEX_COL).drop_duplicates()) > 1:
+        # now create col to update according to prev pr height if needed
+        # only one chromosome (no matter how many pr)
+        if len(genesmd_df[CHROM_COL].drop_duplicates()) == 1:
+            genesmd_df = genesmd_df.assign(
+                upd_yc=genesmd_df.groupby([PR_INDEX_COL], group_keys=False).ngroup(
+                    ascending=False
+                )
+            )
+            genesmd_df["ycoord"] += genesmd_df["upd_yc"]
+
+        # +1 chromosome and +1 pr
+        elif len(genesmd_df.index.get_level_values(PR_INDEX_COL).drop_duplicates()) > 1:
             genesmd_df = genesmd_df.assign(
                 upd_yc=genesmd_df.groupby(CHROM_COL, group_keys=False).apply(
                     lambda x: x.groupby(PR_INDEX_COL).ngroup(ascending=False)
                 )
             )
             genesmd_df["ycoord"] += genesmd_df["upd_yc"]
-        ### now create col to update according to prev pr height
+
         genesmd_df.reset_index(PR_INDEX_COL, inplace=True)
 
     # Assign color to each gene
